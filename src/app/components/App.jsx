a0c872bc93d04ced65448ae031c599c86debff1a
Leandre Perrot <leperrot@dpt_info.iut.local>
2018-02-06 17:18:05 +0100

    fin des tests, début RéAcT, affichage d'une liste d'episodes

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -9,25 +9,69 @@ import configure from './store';
 
 const store = configure();
 
-class Yolo extends Component {
-    render() {
-        return(<h1>Hello World  !!</h1>);
-    }
-};
+class EpisodeItemComponent extends Component {
 
-class Swag extends Component {
-    render() {
-        return(<h1>Alexis le technicien de surface</h1>);
+    constructor(props){
+        super(props);
+
+        this.state = {
+            episodes: []
+        };
     }
-};
+
+    componentDidMount(){
+        fetch("/api/episodes")
+            .then(function(response){
+                if (response.status >= 400) {
+                    throw new Error("Bad response from server");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({ episodes: data });
+            });
+    }
+
+    render(){
+        let episodes = this.state.episodes;
+        return(
+        <table>
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Code</td>
+                    <td>Note</td>
+                </tr>
+            </thead>
+            <tbody>
+                    {episodes.map(episode =>
+                        <tr>
+                            <td>{JSON.parse(episode).name}</td>
+                            <td>{JSON.parse(episode).code}</td>
+                            <td>{JSON.parse(episode).note}</td>
+                        </tr>
+                    )}
+            </tbody>
+        </table>);
+    }
+}
+
+class Swag extends Component{
+    render(){
+        return(
+            <h1>Bonjour</h1>
+        );
+    }
+}
+
 
 export default class App extends Component {
-    render() {
+    render(){
         return (
             <Provider store={store}>
                 <Router>
                   <div>
-                    <Route path="/" component={Yolo}>
+                    <Route path="/" component={EpisodeItemComponent}>
                     </Route>
                     <Route path="/new" component={Swag}>
                     </Route>
--- tests/server/api_spec2.js
+++ tests/server/api_spec2.js
@@ -2,11 +2,40 @@
  * Created by leperrot on 30/01/18.
  */
 const frisby = require('frisby');
+var fs = require('fs');
 const Joi = frisby.Joi;
+var config = require('/home/etud/leperrot/edu-project-template-2018/src/server/config.js');
 
 const URL = 'http://localhost:'+process.env.SERVER_PORT+'/api/episodes';
 
+function createFakeEp() {
+    let episode = {
+        name:"Sherlock",code:"S01E01",note:"9.8",id:"9bade1e5-617d-4407-9a60-7a9912a24b0c"
+    };
+    fs.writeFile(config.data+"/"+episode.id+'.json',JSON.stringify(episode), "UTF-8", function(err){
+        if(err){
+            return;
+        }
+    });
+    return episode;
+}
+
 describe("1st", function(){
+
+    afterAll(function(){
+        fs.readdir(config.data, function(error, files){
+            if(error) {
+                console.log(error);
+                return;
+            }
+            files.forEach(function(file){
+                fs.unlink(config.data + "/" + file, function (error) {
+                    if(error)
+                        console.log(error);
+                });
+            });
+        });
+    });
 
     it("should retrieve all the episodes", function(){
         frisby
@@ -27,6 +56,37 @@ describe("1st", function(){
             });
     });
 
+    describe("2nd", function () {
 
+        let episode;
 
+        beforeEach(function () {
+            episode = createFakeEp();
+        });
+
+        it("should get an episode", function(){
+            frisby
+                .get(URL+'/'+episode.id)
+                .expect("status", 200)
+                .expect("json", "name", "Sherlock")
+        });
+
+        it("should modify an episode", function(){
+            frisby
+                .put(URL+'/'+episode.id, {
+                    name:"Sherlock",
+                    code:"S01E01",
+                    note:"10",
+                    id:episode.id
+                })
+                .expect("status", 200)
+                .expect("json", "note", "10")
+        });
+
+        it("should delete the episode", function () {
+            frisby
+                .del(URL+'/'+episode.id)
+                .expect("status", 200)
+        });
+    });
 });
