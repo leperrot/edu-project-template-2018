5c265a91fae5a99af443905f557e3f636bc7b7a0
Leandre Perrot <leperrot@dpt_info.iut.local>
2018-02-09 14:03:00 +0100

    Component toussa toussa mais post cest du caca

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -9,7 +9,24 @@ import configure from './store';
 
 const store = configure();
 
-class EpisodeItemComponent extends Component {
+class EpisodeItemComponent extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <tr>
+                <td>{this.props.episode.name}</td>
+                <td>{this.props.episode.code}</td>
+                <td>{this.props.episode.note}</td>
+            </tr>
+        );
+    }
+}
+
+class EpisodeListComponent extends Component {
 
     constructor(props){
         super(props);
@@ -35,35 +52,99 @@ class EpisodeItemComponent extends Component {
     render(){
         let episodes = this.state.episodes;
         return(
-        <table>
-            <thead>
-                <tr>
-                    <td>Nom</td>
-                    <td>Code</td>
-                    <td>Note</td>
-                </tr>
-            </thead>
-            <tbody>
-                    {episodes.map(episode =>
-                        <tr>
-                            <td>{JSON.parse(episode).name}</td>
-                            <td>{JSON.parse(episode).code}</td>
-                            <td>{JSON.parse(episode).note}</td>
-                        </tr>
-                    )}
-            </tbody>
-        </table>);
-    }
-}
-
-class Swag extends Component{
-    render(){
-        return(
-            <h1>Bonjour</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nom</td>
+                        <td>Code</td>
+                        <td>Note</td>
+                    </tr>
+                </thead>
+                <tbody>
+                        {episodes.map(episode =>
+                            <EpisodeItemComponent episode={JSON.parse(episode)}/>
+                        )}
+                </tbody>
+            </table>
         );
     }
 }
 
+class EpisodeFormComponent extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            code: "",
+            note: 0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        fetch("/api/episodes", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        }).then(function(response){
+            window.location.reload();
+        });
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render(){
+        return(
+            <form method="post" onSubmit={this.handleSubmit}>
+                <label for="name">Serie</label>
+                <br/>
+                <input type="text" id="name" name="name" value={this.state.name} placeholder="Serie" onChange={this.handleChange}/>
+                <br/>
+                <label for="code">Code Episode</label>
+                <br/>
+                <input type="text" id="code" name="code" value={this.state.code} placeholder="S00E00" onChange={this.handleChange}/>
+                <br/>
+                <label for="note">Note</label>
+                <br/>
+                <input type="number" id="note" name="note" min="0" max="10" value={this.state.note} onChange={this.handleChange}/>
+                <br/>
+                <input type="submit" value="Watched"/>
+            </form>
+        )
+    }
+}
+
+class EpisodeComponent extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <EpisodeListComponent/>
+                <br/>
+                <EpisodeFormComponent/>
+            </div>
+        );
+    }
+
+}
 
 export default class App extends Component {
     render(){
@@ -71,9 +152,7 @@ export default class App extends Component {
             <Provider store={store}>
                 <Router>
                   <div>
-                    <Route path="/" component={EpisodeItemComponent}>
-                    </Route>
-                    <Route path="/new" component={Swag}>
+                    <Route path="/" component={EpisodeComponent}>
                     </Route>
                   </div>
                 </Router>
